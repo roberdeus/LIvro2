4acce466d848b68aa62d20b186b4bcc245ab3a90
roberdeus <robertofilho89089@gmail.com>
2019-08-15 11:58:09 -0300

    correção

--- src/aula2/livro.java
+++ src/aula2/livro.java
@@ -1,14 +1,60 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package aula2;
 
-/**
- *
- * @author Usuário
- */
 public class livro {
+    private String titulo;
+    private String autor;
+    private String espn;
+    private int paginas;
+    private String ano;
+    private String editora;
+    private int valor;
     
+    public void settitulo(String titulo) {
+            this.titulo = titulo;
+    }
+    public String settitulo{
+             return this.titulo;
 }
+    
+     public void getautor(String autor) {
+            this.autor = autor;
+    }
+    public String setautor{
+             return this.autor;
+}
+     public void getespn(String espn) {
+            this.espn = espn;
+    }
+    public String setespn{
+             return this.espn;  
+}
+    
+     public void paginas(String qntddepag) {
+            this.paginas = paginas;
+    }
+     
+     public String paginas{
+             return this.paginas;  
+}
+      public void getano(String ano) {
+            this.ano = ano;
+    }
+     
+     public String ano{
+             return this.ano;  
+}
+      public void editora(String editora) {
+            this.editora = editora;
+    }
+     
+     public String editora{
+             return this.editora; 
+    }
+      public void getvalor(String valor) {
+            this.valor = valor;
+    }
+     
+     public String valor{
+             return this.valor;
+    }
